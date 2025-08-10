aaaea9a6b1c35ea63653adc7a6bc661b295430ce
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-08-10 22:46:37 +0530

    chore: remove useless code

--- apps/backend/src/controllers/test.js
+++ apps/backend/src/controllers/test.js
@@ -2,12 +2,8 @@ import { client } from "../services/redis.js";
 
 export const getTest = async (req, res) => {
   console.log("GOTTEM: ", req.method);
-  await client.set("method", req.method);
-  const res1 = await client.xAdd("req:data", "*", {
+  await client.xAdd("req:data", "*", {
     method: req.method
   });
-  console.log("req:test:id", res1);
-  console.log("req:test:length", await client.xLen("req:test"));
-  // Send the data to Redis
   res.send();
 };
